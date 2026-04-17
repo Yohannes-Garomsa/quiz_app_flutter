20e63b1d97c7fb6034b1e1a60d2aa04654f9d7cb
Yohannes-Garomsa <yohannesgaromsa051@gmail.com>
2026-04-17 20:23:08 +0300

    bring material widget to quiz.dart

---
+++ lib/quiz.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.deepPurple.shade197,
+                const Color.fromARGB(252, 91, 8, 234),
+                const Color.fromARGB(252, 162, 5, 197),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
