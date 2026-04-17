1d73716659dcbb04f7048dbfc265766a11858d15
Yohannes-Garomsa <yohannesgaromsa051@gmail.com>
2026-04-17 23:41:13 +0300

    banch of questions created

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -1,16 +1,114 @@
 import 'package:adv_basics/models/quiz_questions.dart';
 
 final questions = [
-  QuizQuestions('what is building block of flutter UIs?', [
-    'Widgets',
-    'Components',
-    'Blocks',
-    'Functions',
+  QuizQuestions('What does HTML stand for?', [
+    'HyperText Markup Language',
+    'HighText Machine Language',
+    'Hyper Transfer Markup Language',
+    'Home Tool Markup Language',
   ]),
-  QuizQuestions('what is flutter?', [
-    'A SDK',
-    'A programming language',
-    'A type of bird',
-    'A framework',
+  QuizQuestions('Which language is used to style web pages?', [
+    'CSS',
+    'HTML',
+    'Python',
+    'Dart',
+  ]),
+  QuizQuestions(
+    'Which language is mainly used to add interactivity to websites?',
+    ['JavaScript', 'HTML', 'SQL', 'C++'],
+  ),
+  QuizQuestions('Which HTML tag is used to create a hyperlink?', [
+    '<a>',
+    '<link>',
+    '<href>',
+    '<p>',
+  ]),
+  QuizQuestions('Which CSS property changes the text color?', [
+    'color',
+    'font-color',
+    'text-style',
+    'background-color',
+  ]),
+  QuizQuestions('Which HTML element is used for the largest heading?', [
+    '<h1>',
+    '<heading>',
+    '<head>',
+    '<h6>',
+  ]),
+  QuizQuestions('Which symbol is used for an id selector in CSS?', [
+    '#',
+    '.',
+    '*',
+    '&',
+  ]),
+  QuizQuestions('Which symbol is used for a class selector in CSS?', [
+    '.',
+    '#',
+    '@',
+    '*',
+  ]),
+  QuizQuestions('What does CSS stand for?', [
+    'Cascading Style Sheets',
+    'Creative Style System',
+    'Computer Style Sheets',
+    'Colorful Style Syntax',
+  ]),
+  QuizQuestions(
+    'Which property is used to change the background color in CSS?',
+    ['background-color', 'color', 'bgcolor', 'background-style'],
+  ),
+  QuizQuestions('Which HTML tag is used to insert an image?', [
+    '<img>',
+    '<image>',
+    '<src>',
+    '<pic>',
+  ]),
+  QuizQuestions('Which attribute provides the image path in an <img> tag?', [
+    'src',
+    'href',
+    'alt',
+    'link',
+  ]),
+  QuizQuestions('Which CSS property is used to make text bold?', [
+    'font-weight',
+    'text-bold',
+    'font-style',
+    'weight',
+  ]),
+  QuizQuestions('Which JavaScript keyword is used to declare a variable?', [
+    'let',
+    'style',
+    'define',
+    'dim',
+  ]),
+  QuizQuestions(
+    'Which method is used to print something in the browser console?',
+    ['console.log()', 'print()', 'echo()', 'log.console()'],
+  ),
+  QuizQuestions('What does DOM stand for?', [
+    'Document Object Model',
+    'Data Object Management',
+    'Digital Ordinance Model',
+    'Document Oriented Method',
+  ]),
+  QuizQuestions(
+    'Which HTTP method is commonly used to get data from a server?',
+    ['GET', 'SEND', 'FETCH', 'PUSH'],
+  ),
+  QuizQuestions(
+    'Which HTTP method is commonly used to send data to a server?',
+    ['POST', 'GET', 'READ', 'LOAD'],
+  ),
+  QuizQuestions('Which HTML tag is used to create an unordered list?', [
+    '<ul>',
+    '<ol>',
+    '<li>',
+    '<list>',
+  ]),
+  QuizQuestions('Which CSS layout system is one-dimensional?', [
+    'Flexbox',
+    'Grid',
+    'Table',
+    'Float',
   ]),
 ];
