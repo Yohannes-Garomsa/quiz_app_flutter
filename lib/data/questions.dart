f4f548cad2be09f3d40b9ca7c7722d4b94246dd6
Yohannes-Garomsa <yohannesgaromsa051@gmail.com>
2026-04-17 23:37:21 +0300

    error fixed

---
+++ lib/data/questions.dart
@@ -0,0 +1,16 @@
+import 'package:adv_basics/models/quiz_questions.dart';
+
+final questions = [
+  QuizQuestions('what is building block of flutter UIs?', [
+    'Widgets',
+    'Components',
+    'Blocks',
+    'Functions',
+  ]),
+  QuizQuestions('what is flutter?', [
+    'A SDK',
+    'A programming language',
+    'A type of bird',
+    'A framework',
+  ]),
+];
