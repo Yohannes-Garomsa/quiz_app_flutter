74b19e3dd454c90155264fafa6dd4561e36e34e3
Yohannes-Garomsa <yohannesgaromsa051@gmail.com>
2026-04-18 14:43:05 +0300

    reusable answer button

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -27,13 +27,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           ),
           const SizedBox(height: 30),
 
-          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
-          const SizedBox(height: 12),
-          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
-          const SizedBox(height: 12),
-          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
-          const SizedBox(height: 12),
-          AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(answerText: item, onTap: () {});
+          }),
         ],
       ),
     );
