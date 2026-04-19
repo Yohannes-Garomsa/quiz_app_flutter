1ba9d7563354028d7804802bf8f0416202270250
Yohannes-Garomsa <yohannesgaromsa051@gmail.com>
2026-04-19 14:16:48 +0300

    answer and question loopped

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.chooseAnswer});
 
   final List<String> chooseAnswer;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chooseAnswer.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chooseAnswer[i],
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
